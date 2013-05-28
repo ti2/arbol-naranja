607cda4d99b793becf08498497b8b7ca574abeba
MaoMantish <mauricio@8manos.com>
2013-05-28 18:42:38 -0500

    tema coming soon

---
+++ wp-content/themes/coming_soon/index.php
@@ -0,0 +1,142 @@
+<html>
+<head>
+<title>&Aacute;RBOL NARANJA</title>
+<!-- AddThis Welcome BEGIN -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4fd046c31276ee70"></script>
+<script type='text/javascript'>
+addthis.bar.initialize({'default':{
+	"backgroundColor": "#000000",
+	"buttonColor": "#098DF4",
+	"textColor": "#FFFFFF",
+	"buttonTextColor": "#FFFFFF"
+},rules:[
+	{
+		"name": "Twitter",
+		"match": {
+			"referringService": "twitter"
+		},
+		"message": "If you find this page helpful:",
+		"action": {
+			"type": "button",
+			"text": "Tweet it!",
+			"verb": "share",
+			"service": "twitter"
+		}
+	},
+	{
+		"name": "Facebook",
+		"match": {
+			"referringService": "facebook"
+		},
+		"message": "Tell your friends about us:",
+		"action": {
+			"type": "button",
+			"text": "Share on Facebook",
+			"verb": "share",
+			"service": "facebook"
+		}
+	},
+	{
+		"name": "Google",
+		"match": {
+			"referrer": "google.com"
+		},
+		"message": "If you like this page, let Google know:",
+		"action": {
+			"type": "button",
+			"text": "+1",
+			"verb": "share",
+			"service": "google_plusone_share"
+		}
+	}
+]});
+</script>
+<!-- AddThis Welcome END -->
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39405153-1']);
+  _gaq.push(['_setDomainName', 'arbolnaranja.com']);
+  _gaq.push(['_setAllowLinker', true]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<style type="text/css">
+.a {
+	text-align: center;
+}
+body,td,th {
+	font-family: Verdana, Geneva, sans-serif;
+	font-size: 12px;
+}
+a:link {
+	color: #333;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #333;
+}
+a:hover {
+	text-decoration: none;
+	color: #333;
+}
+a:active {
+	text-decoration: none;
+	color: #333;
+	font-family: Verdana, Geneva, sans-serif;
+	font-weight: normal;
+}
+.SUB {
+	font-weight: bold;
+	font-style: italic;
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- Save for Web Slices (photo.jpg) -->
+<br>
+<table width="322" border="0" align="center">
+  <tr>
+    <td align="center"><img src="<?php bloginfo('template_directory'); ?>/index.jpg" width="256" height="256" alt=""></td>
+  </tr>
+  <tr>
+    <td height="126" align="center"><p><span title="Calle 60 # 17-20 Bogotá;, Cundinamarca">Producci&oacute;n, Estudio de Grabaci&oacute;n, Alquiler de backline,  Salas de Ensayo, Booking, Management  y Montaje de eventos.<br>
+    </span><span title="Calle 60 # 17-20 Bogota;, Cundinamarca"><a href="https://www.google.com/url?sa=D&oi=plus&q=https://maps.google.com/maps?ie%3DUTF8%26cid%3D12909657004657128515%26q%3D%25C3%2581rbol%2BNaranja%26iwloc%3DA%26gl%3DCO%26hl%3Den_uk" target="_blank" tabindex="0"><br>
+        <span class="SUB">[<em>57</em>] (<em>1</em>) 744-1835</span><br>
+        Calle 60 # 17-20</a> &bull; Bogot&aacute;, Cundinamarca</span><br>
+    </p></td>
+  </tr>
+  <tr>
+    <td align="center"><div class="g-plus" data-height="69" data-href="//plus.google.com/104366939675377139204" data-rel="author"></div>
+</td>
+  </tr>
+  <tr align="center" valign="middle" class="a">
+    <td></td>
+  </tr>
+</table>
+<br><center>
+
+<!-- Place this tag where you want the badge to render. -->
+
+<!-- Place this tag after the last badge tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+<!-- End Save for Web Slices -->
+</body>
+</html>
