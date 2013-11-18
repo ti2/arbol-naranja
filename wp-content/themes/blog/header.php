7c33809dd31b022ca29362eb744a595c5c230dd9
MaoMantish <mauricio@8manos.com>
2013-11-18 18:45:11 -0500

    nueva fuente

--- wp-content/themes/blog/header.php
+++ wp-content/themes/blog/header.php
@@ -9,6 +9,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
+	<link href='http://fonts.googleapis.com/css?family=Enriqueta:400,700' rel='stylesheet' type='text/css'>
 	<link href="<?php bloginfo('template_directory'); ?>/css/normalize.css" rel="stylesheet" media="all">
 	<link href="<?php bloginfo('template_directory'); ?>/css/main.css" rel="stylesheet" media="all">
 
