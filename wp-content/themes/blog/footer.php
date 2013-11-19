80cf824344d611aef9250da5e11d6cbc41837b3b
MaoMantish <mauricio@8manos.com>
2013-11-19 18:25:03 -0500

    siempre se comparte la url del home

--- wp-content/themes/blog/footer.php
+++ wp-content/themes/blog/footer.php
@@ -11,12 +11,14 @@
 
 		<div class="main-block social-sharing">
 			<h4>Compartir:</h4>
+
+			<?php $share_url = urlencode(home_url()); ?>
 			<!-- google plus -->
-			<a href="#" onclick="window.open('https://plus.google.com/share?url='+encodeURIComponent(location.href), 'googleplus-share-dialog', 'width=600,height=450'); return false;">Google +</a>
+			<a href="https://plus.google.com/share?url=<?php echo $share_url; ?>" onclick="window.open(this.href, 'googleplus-share-dialog', 'width=600,height=450'); return false;">Google +</a>
 			<!-- facebook share -->
-			<a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href), 'facebook-share-dialog', 'width=626,height=436'); return false;">Facebook</a>
+			<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $share_url; ?>" onclick="window.open(this.href, 'facebook-share-dialog', 'width=626,height=436'); return false;">Facebook</a>
 			<!-- twitter -->
-			<a href="#" onclick="window.open('https://twitter.com/share?url='+encodeURIComponent(location.href), 'twitter-share-dialog', 'width=550,height=450'); return false;">Twitter</a>
+			<a href="https://twitter.com/share?url=<?php echo $share_url; ?>" onclick="window.open(this.href, 'twitter-share-dialog', 'width=550,height=450'); return false;">Twitter</a>
 		</div>
 	</div><!-- #articles -->
 
